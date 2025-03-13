a4fc853d9105c544dff4749dd7d54d075980b87a
student.moringaschool.com <guy.nkurunziza@student.moringaschool.com>
2025-03-13 17:00:30 +0200

    functions lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(position) {
+  return Math.abs(position - 42);
+}
+
+function distanceFromHqInFeet(position) {
+  return distanceFromHqInBlocks(position) * 264;
+}
+
+function distanceTravelledInFeet(depart, arrivee) {
+  return Math.abs(arrivee - depart) * 264;
+}
+
+function calculatesFarePrice(depart, arrivee) {
+  const distance = distanceTravelledInFeet(depart, arrivee);
+
+  if (distance <= 400) {
+    return 0;
+  } else if (distance <= 2000) {
+    return (distance - 400) * 0.02;
+  } else if (distance <= 2500) {
+    return 25;
+  } else {
+    return "cannot travel that far";
+  }
+}
